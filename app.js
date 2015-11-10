72aaa325468d8bb042d687d004709c8e35bd5828
Jeffrey Yang <jeffycyang@gmail.com>
2015-11-10 11:56:33 -0800

    adds error message for invalid calculations and moves extra files to extra folder

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.controller('appCtrl',function($scope){
   $scope.solution;
 
   $scope.hasCalc=false;
+  $scope.isValid=true;
 
   $scope.addPoint=function(){
   	$scope.xVals.push($scope.x_val);
@@ -18,7 +19,12 @@ app.controller('appCtrl',function($scope){
   	$scope.funcType=$scope.ftype;
   	console.log("fuck you");
   	console.log($scope.funcType);
-  	if($scope.xVals.length!==0&&$scope.yVals.length!==0){
+  	if($scope.order>$scope.xVals.length){
+  	  $scope.isValid=false;
+  	}else{
+  	  $scope.isValid=true;
+  	}
+  	if($scope.xVals.length!==0&&$scope.yVals.length!==0&&$scope.isValid){
   	  $scope.solution=leastSqr(0,$scope.xVals,$scope.yVals,$scope.order);
   	}
   };
